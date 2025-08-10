c3aa3144e474eee0fc7822163cd1c449b250426e
prapti299 <praptiacharya05@gmail.com>
2025-08-10 10:53:08 +0530

    Add files via upload

---
+++ multiplypolynomial.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void addpolynomial(int c[],int d[],int g){
+	for(int i=0;i<g;i++){
+		int t= c[i]*d[i];
+		if(i==0){
+			printf("%d+",t);
+		}
+		else{
+			if(i==g-1){
+				printf("%dX^%d",t,i);
+			}else{
+		        printf("%dX^%d+",t,i);
+		    }
+		}
+		t=0;
+	}
+}
+int main(){
+	int r,c,i,j;
+	printf("enter rows and column:");
+	scanf("%d %d ",&r,&c);
+	 int max = (r > c) ? r : c;
+    int a[max], b[max];
+    for (i = 0; i < max; i++) {
+        a[i] = 1;
+        b[i] = 1;
+    }
+	for(i=0;i<r;i++){
+		scanf("%d",&a[i]);
+	}
+	for(j=0;j<c;j++){
+		scanf("%d",&b[j]);
+	}
+    addpolynomial(a,b,max);
+	return 0;
+}
